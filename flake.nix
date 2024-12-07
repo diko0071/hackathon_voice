c84e7c9a3a9893f048bef6ec662f23cc82182df2
drawnwren <drawnwren@gmail.com>
2024-12-07 11:50:42 -0800

    add flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,74 @@
 {
-  description = "A dev shell that provides a Python environment from requirements.txt using mach-nix";
-
+  description = "Create Nix development environment";
   inputs = {
-    # Pin the Nixpkgs version you want. Example uses NixOS 23.05.
-
-    # mach-nix input
-    mach-nix.url = "github:DavHau/mach-nix";
-
-    nixpkgs.follows = "mach-nix/nixpkgs";
-    # Optionally, you can specify a ref or rev to pin a specific mach-nix version
-    # mach-nix.rev = "<commit hash>";
-    # mach-nix.ref = "refs/tags/<version tag>";
-  };
-
-  outputs = { self, nixpkgs, mach-nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShell {
-        # Create a Python environment using mach-nix and requirements.txt
-        buildInputs = [
-          (mach-nix.mkPythonShell {
-            python = pkgs.python310; 
-            requirements = builtins.readFile ./requirements.txt;
-          })
-        ];
-      };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pyproject-nix = {
+      url = "github:nix-community/pyproject.nix";
+      flake = false;
     };
-}
+  };
+  outputs = { self, nixpkgs, flake-utils, pyproject-nix }:
+    let
+      pyproject = import (pyproject-nix + "/lib") { inherit (nixpkgs) lib; };
+      # Load/parse requirements.txt
+      project = pyproject.project.loadRequirementsTxt {
+        requirements = ./requirements.txt;
+      };
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python311;
+        
+        # Create a custom Python environment with pip
+        pythonWithPackages = python.withPackages (ps: with ps; [
+          pip
+          virtualenv
+        ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+              pkgs.postgresql
+          ];
+          packages = [
+            pythonWithPackages
+            # PostgreSQL dependencies
+            pkgs.postgresql
+            pkgs.pkg-config
+            # Pillow dependencies
+            pkgs.libjpeg
+            pkgs.zlib
+            pkgs.libtiff
+            pkgs.freetype
+            pkgs.lcms2
+            pkgs.libwebp
+            pkgs.tcl
+            pkgs.tk
+            pkgs.harfbuzz
+            pkgs.fribidi
+            pkgs.libraqm
+            # OpenSSL
+            pkgs.openssl
+            pkgs.pyright
+          ];
+          
+          shellHook = ''
+            # Create and activate a virtual environment if it doesn't exist
+            if [ ! -d .venv ]; then
+              ${pythonWithPackages}/bin/python -m venv .venv
+            fi
+            source .venv/bin/activate
 
+            # Set up environment variables for Pillow
+            export LDFLAGS="$LDFLAGS -L${pkgs.libjpeg.out}/lib"
+            export CPPFLAGS="$CPPFLAGS -I${pkgs.libjpeg.dev}/include"
+
+            if [ -f requirements.txt ]; then
+              pip install -r requirements.txt
+            fi
+          '';
+        };
+      });
+}
