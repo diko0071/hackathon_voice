a9d65805a3648669dbbeb409849d29dfd513cb4e
Dmitry Korzhov <korzhov.work2019@gmail.com>
2024-12-07 11:59:38 -0800

    finsihed models

--- backend/lesson/models.py
+++ backend/lesson/models.py
@@ -3,34 +3,34 @@ from django.db import models
 # Create your models here.
 class Lesson(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Lecture(models.Model):
+class LessonLecture(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    description = models.TextField()
-    video_url = models.URLField()
-    duration = models.IntegerField()
+    content = models.TextField(null=True, blank=True)
+    video_url = models.URLField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class LessonSection(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class LessonMaterial(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     section = models.ForeignKey(LessonSection, on_delete=models.CASCADE)
-    content = models.TextField()
+    content = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class LessonQuiz(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -39,9 +39,6 @@ class LessonQuizQuestion(models.Model):
     quiz = models.ForeignKey(LessonQuiz, on_delete=models.CASCADE)
     question = models.TextField()
     options = models.JSONField()
-    right_answer = models.TextField()
+    right_answer = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-
