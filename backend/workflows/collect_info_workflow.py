c33a82d3db19678d87c6579a731f9319bf1a0602
drawnwren <drawnwren@gmail.com>
2024-12-07 13:53:32 -0800

    x

--- backend/workflows/collect_info_workflow.py
+++ backend/workflows/collect_info_workflow.py
@@ -1,6 +1,8 @@
 from temporalio import workflow
 from datetime import timedelta
 from typing import List, Dict
+import pymongo
+
 
 @workflow.defn
 class CollectInfoWorkflow:
@@ -9,13 +11,18 @@ class CollectInfoWorkflow:
         queries = await workflow.execute_activity(
             "generate_queries",
             args=[title, description],
-            start_to_close_timeout=timedelta(minutes=2)
+            start_to_close_timeout=timedelta(minutes=2),
         )
-        
+
         results = await workflow.execute_activity(
             "scrape_content",
             args=[queries],
-            start_to_close_timeout=timedelta(minutes=5)
+            start_to_close_timeout=timedelta(minutes=5),
         )
-        
+
+        # put results into mongodb
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        client["tavily_results"].insert_many(results)
+
         return results
+
